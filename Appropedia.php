a9604807beef7146ec1fb7ba7cbd7848c400f028
Sophivorus <schenonef@gmail.com>
2023-04-24 06:22:17 -0700

    Hide more messages

--- Appropedia.php
+++ Appropedia.php
@@ -33,7 +33,9 @@ class Appropedia {
 			case 'disclaimers': // Part of the footer, see onSkinAddFooterLinks below
 			case 'histlegend': // @todo Hide via CSS
 			case 'newarticletext':
-			case 'prefs-help-realname':
+			case 'createacct-username-help': // Special:CreateAccount
+			case 'createacct-useuniquepass': // Special:CreateAccount
+			case 'prefs-help-realname': // Special:CreateAccount and Special:Preferences
 			case 'hcaptcha-createaccount': // Extension:ConfirmEdit
 			case 'hcaptcha-edit': // Extension:ConfirmEdit
 			case 'upload-form-label-not-own-work-local-generic-local': // Upload dialog of Extension:VisualEditor

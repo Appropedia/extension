e37852ef0e69d992f64ad5d296fca332f4f78486
Sophivorus <schenonef@gmail.com>
2023-04-26 06:48:04 -0700

    Split PHP code into 3 files

---
+++ AppropediaMessages.php
@@ -0,0 +1,125 @@
+<?php
+
+class AppropediaMessages {
+
+	/**
+	 * Customize interface messages
+	 *
+	 * When Appropedia updates to MediaWiki 1.41+
+	 * some of this code should migrate to the MessageCacheFetchOverrides hook instead
+	 * see https://www.mediawiki.org/wiki/Manual:Hooks/MessageCacheFetchOverrides
+	 */
+	public static function onMessagesPreLoad( $title, &$message, $code ) {
+		if ( $code === 'qqx' ) {
+			return;
+		}
+
+		$parts = explode( '/', $title );
+		$key = $parts[0];
+		$key = strtolower( $key );
+		switch ( $key ) {
+
+			// Unwanted stuff is generally hidden via CSS
+			// but the following messages are hidden by making them empty
+			// because there's no easy way to target them via CSS
+			// or some other technical reason
+			case 'privacy': // Part of the footer, see onSkinAddFooterLinks below
+			case 'disclaimers': // Part of the footer, see onSkinAddFooterLinks below
+			case 'histlegend': // @todo Hide via CSS
+			case 'newarticletext':
+			case 'createacct-username-help': // Special:CreateAccount
+			case 'createacct-useuniquepass': // Special:CreateAccount
+			case 'prefs-help-realname': // Special:CreateAccount and Special:Preferences
+			case 'hcaptcha-createaccount': // Extension:ConfirmEdit
+			case 'hcaptcha-edit': // Extension:ConfirmEdit
+			case 'upload-form-label-not-own-work-local-generic-local': // Upload dialog of Extension:VisualEditor
+				$message = '';
+				break;
+
+			/**
+			 * Override messages
+			 */
+
+			case 'copyrightwarning':
+				$message = wfMessage( "appropedia-page-edit-warning" )->text();
+				break;
+
+			case 'anoneditwarning':
+				$message = wfMessage( "appropedia-anon-edit-warning" )->text();
+				break;
+
+			case 'editnotice-2':
+				$context = RequestContext::getMain();
+				$title = $context->getTitle();
+				if ( $title->isSubpage() ) {
+					break;
+				}
+				$root = $title->getRootTitle();
+				$user = $context->getUser()->getUserPage();
+				if ( $root->equals( $user ) ) {
+					break;
+				}
+				$link = $title->getTalkPage()->getFullURL( [ 'action' => 'edit', 'section' => 'new' ] );
+				$message = wfMessage( 'appropedia-user-edit-warning', $link )->text();
+				break;
+
+			case 'editnotice-8':
+				$page = 'Appropedia:UI'; // @todo Should probably be defined elsewhere
+				$message = wfMessage( 'appropedia-interface-edit-warning', $page )->text();
+				break;
+
+			case 'editnotice-10':
+				$page = 'Appropedia:Templates'; // @todo Should probably be defined elsewhere
+				$message = wfMessage( 'appropedia-template-edit-warning', $page )->text();
+				break;
+
+			case 'categorytree-member-num':
+				$message = "($4)";
+				break;
+
+			case 'noarticletext':
+				$context = RequestContext::getMain();
+				$title = $context->getTitle();
+				$namespace = $title->getNamespace();
+				$action = in_array( $namespace, [ 0, 2, 4, 12 ] ) ? 'veaction' : 'action';
+				$preload = $namespace === 2 ? 'Preload:User' : null;
+				$link = $title->getFullURL( [ $action => 'edit', 'preload' => $preload ] );
+				$text = wfMessage( 'appropedia-create-page' )->text();
+				$message = '[' . $link . '<span class="mw-ui-button mw-ui-progressive">' . $text . '</span>]';
+				break;
+
+			case 'welcomecreation-msg':
+				$message = wfMessage( 'appropedia-account-created' )->text();
+				$context = RequestContext::getMain();
+				$link = $context->getUser()->getUserPage()->getFullURL( [ 'veaction' => 'edit', 'preload' => 'Preload:User' ] );
+				$text = wfMessage( 'appropedia-create-user-page' )->text();
+				$message .= "\n\n[" . $link . '<span class="mw-ui-button mw-ui-progressive">' . $text . '</span>]';
+				break;
+
+			case 'searchmenu-exists':
+				$message = '<span class="mw-search-exists-title">{{TARGET|$1}}</span>
+				<span class="mw-search-exists-image">{{#if:{{#show:$1|?Image#}}|[[{{#show:$1|?Image#}}|300px|link=]]}}</span>
+				<span class="mw-search-exists-text">{{#invoke:Transcluder|main|$1#|only=paragraphs|paragraphs=1|templates=0|references=0|noLinks=true}}</span>
+				<span class="mw-search-exists-more">[[{{TARGET|$1}}|Read more »]]</span>';
+				break;
+
+			/**
+			 * Override extension and skin messages
+			 * These require setting the language explictly
+			 */
+
+			case 'mwe-upwiz-add-file-0-free':
+				$message = wfMessage( 'appropedia-select-files' )->inLanguage( $code )->text();
+				break;
+
+			case 'upload-form-label-not-own-work-message-generic-local':
+				$page = 'Special:UploadWizard';
+				$message = wfMessage( 'appropedia-not-own-work', $page )->inLanguage( $code )->text();
+				break;
+
+			case 'poncho-print':
+				$message = wfMessage( 'appropedia-download-pdf' )->inLanguage( $code )->text();
+				break;
+		}
+	}
+}
---
+++ AppropediaSearch.php
@@ -0,0 +1,259 @@
+<?php
+
+/**
+ * This class contains all PHP code related to search customization
+ * (there's some relevant JavaScript code at Appropedia.js too)
+ */
+class AppropediaSearch {
+
+	/**
+	 * Customize the content of search results
+	 */
+	public static function onShowSearchHit( $searchPage, $result, $terms, &$link, &$redirect, &$section, &$extract, &$score, &$size, &$date, &$related, &$html ) {
+
+		// Remove time from date, per mostly useless and noisy (only works in English and some other languages)
+		$date = substr( $date, strpos( $date, ', ' ) + 1 );
+	}
+
+	/**
+	 * Customize the title of search results
+	 */
+	public static function onShowSearchHitTitle( Title &$title, &$titleSnippet, SearchResult $result, $terms, SpecialSearch $specialSearch, array &$query, array &$attributes ) {
+
+		// Show the display title rather than the title
+		// See https://phabricator.wikimedia.org/T65975
+		$dbr = wfGetDB( DB_REPLICA );
+		$displayTitle = $dbr->selectField( 'page_props', 'pp_value', [ 'pp_propname' => 'displaytitle', 'pp_page' => $title->getArticleId() ] );
+		if ( $displayTitle ) {
+			$titleSnippet = $displayTitle;
+		}
+	}
+
+	/**
+	 * Cusomize the default search profile
+	 */
+	public static function onSpecialPageBeforeExecute( $special ) {
+		if ( $special->getName() === 'Search' ) {
+			$request = $special->getRequest();
+			$profile = $request->getText( 'profile' );
+			if ( !$profile ) {
+				$request->setVal( 'profile', 'pages' );
+			}
+		}
+	}
+
+	/**
+	 * Customize the search profiles
+	 */
+	public static function onSpecialSearchProfiles( &$profiles ) {
+		$profiles = [
+			'pages' => [
+				'message' => 'appropedia-searchprofile-pages',
+				'tooltip' => 'searchprofile-articles-tooltip',
+				'namespaces' => [ NS_MAIN ],
+				'namespace-messages' => [ 'content pages' ]
+			],
+			'files' => [
+				'message' => 'appropedia-searchprofile-files',
+				'tooltip' => 'searchprofile-articles-tooltip',
+				'namespaces' => [ NS_FILE ],
+				'namespace-messages' => [ 'files' ]
+			],
+			'users' => [
+				'message' => 'appropedia-searchprofile-users',
+				'tooltip' => 'searchprofile-articles-tooltip',
+				'namespaces' => [ NS_USER ],
+				'namespace-messages' => [ 'user pages' ]
+			],
+			'talks' => [
+				'message' => 'appropedia-searchprofile-talks',
+				'tooltip' => 'searchprofile-articles-tooltip',
+				'namespaces' => [
+					NS_TALK,
+					NS_USER_TALK,
+					NS_PROJECT_TALK,
+					NS_MEDIAWIKI_TALK,
+					NS_TEMPLATE_TALK,
+					NS_HELP_TALK,
+					NS_PRELOAD_TALK,
+					NS_CATEGORY_TALK,
+					275, // Widget talk
+					829, // Lua modules talk
+				],
+				'namespace-messages' => [ 'talk pages' ]
+			],
+			'other' => [
+				'message' => 'appropedia-searchprofile-other',
+				'tooltip' => 'searchprofile-articles-tooltip',
+				'namespaces' => [
+					NS_PROJECT,
+					NS_MEDIAWIKI,
+					NS_TEMPLATE,
+					NS_HELP,
+					NS_PRELOAD,
+					NS_CATEGORY,
+					274, // Widgets
+					828, // Lua modules
+				],
+				'namespace-messages' => [ 'categories, templates, widgets, Lua modules, help pages, preloads, Appropedia and MediaWiki namespaces' ]
+			],
+		];
+	}
+
+	/**
+	 * Cusomize the search profile form
+	 *
+	 * This new markup interacts with Appropedia.js
+	 */
+	public static function onSpecialSearchProfileForm( $special, &$form, &$profile, $term, array $opts ) {
+		$form = '<div class="mw-search-profile-form">';
+		$request = $special->getRequest();
+		$search = $request->getText( 'search' );
+		$terms = explode( ' ', $search );
+
+		// Content pages
+		if ( $profile === 'pages' ) {
+
+			// Page type
+			$form .= '<select id="search-filter-page-type" style="width: 55px;">';
+			$form .= '<option value="">Any type</option>';
+			$options = [
+				'Projects',
+				'Devices',
+				'Organizations',
+				'Papers',
+				'Books',
+				'Literature reviews'
+			];
+			foreach ( $options as $text ) {
+				$value = 'incategory:' . str_replace( ' ', '_', $text );
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+
+			// Sustainable Development Goal
+			$form .= '<select id="search-filter-page-sdg" style="width: 58px;">';
+			$form .= '<option value="">Any SDG</option>';
+			$options = [
+				'SDG01 No poverty',
+				'SDG02 Zero hunger',
+				'SDG03 Good health and well-being',
+				'SDG04 Quality education',
+				'SDG05 Gender equality',
+				'SDG06 Clean water and sanitation',
+				'SDG07 Affordable and clean energy',
+				'SDG08 Decent work and economic growth',
+				'SDG09 Industry innovation and infrastructure',
+				'SDG10 Reduced inequalities',
+				'SDG11 Sustainable cities and communities',
+				'SDG12 Responsible consumption and production',
+				'SDG13 Climate action',
+				'SDG14 Life below water',
+				'SDG15 Life on land',
+				'SDG16 Peace justice and strong institutions',
+				'SDG17 Partnerships for the goals',
+			];
+			foreach ( $options as $text ) {
+				$value = 'incategory:' . str_replace( ' ', '_', $text );
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+
+			// Language
+			$form .= '<select id="search-filter-page-language" style="width: 87px;">';
+			$form .= '<option value="">Any language</option>';
+			$options = [
+				'English' => 'en',
+				'French' => 'fr',
+				'German' => 'de',
+				'Italian' => 'it',
+				'Spanish' => 'es',
+				'Portuguese' => 'pt',
+			];
+			foreach ( $options as $text => $value ) {
+				$value = 'inlanguage:'. $value;
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+		}
+
+		// File pages
+		if ( $profile === 'files' ) {
+
+			// File type
+			$filetype = strtolower( $filetype );
+			$form .= '<select id="search-filter-file-type" style="width: 55px;">';
+			$form .= '<option value="">Any type</option>';
+			$options = [
+				'Images' => 'bitmap',
+				'Drawings' => 'drawing',
+				'Documents' => 'office',
+				'Code' => 'text',
+				'Videos' => 'video',
+				'Audios' => 'audio',
+			];
+			foreach ( $options as $text => $value ) {
+				$value = 'filetype:' . $value;
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+
+			// File MIME
+			$filemime = $request->getText( 'filemime' );
+			$form .= '<select id="search-filter-file-mime" style="width: 69px;">';
+			$form .= '<option value="">Any format</option>';
+			$options = [
+				'JPG' => 'image/jpeg',
+				'PNG' => 'image/png',
+				'GIF' => 'image/gif',
+				'SVG' => 'image/svg+xml',
+				'PDF' => 'application/pdf',
+			];
+			foreach ( $options as $text => $value ) {
+				$value = 'filemime:' . $value;
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+
+			// File license
+			$form .= '<select id="search-filter-file-license" style="width: 72px;">';
+			$form .= '<option value="">Any license</option>';
+			$options = [
+				'CC-BY-SA-4.0' => 'CC-BY-SA-4.0 files',
+				'CC-BY-SA-3.0' => 'CC-BY-SA-3.0 files',
+				'CC-BY-SA-2.0' => 'CC-BY-SA-2.0 files',
+				'CC0-1.0' => 'CC0-1.0 files',
+				'GFDL' => 'GFDL files',
+				'GPL' => 'GPL files',
+				'LGPL' => 'LGPL files',
+				'Public domain' => 'Public domain files',
+			];
+			foreach ( $options as $text => $value ) {
+				$value = 'incategory:' . $value;
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+		}
+
+		if ( $profile === 'users' ) {
+
+			// User location
+			$location = $request->getText( 'location' );
+			$form .= '<select id="search-filter-user-location" style="width: 77px;">';
+			$form .= '<option value="">Any location</option>';
+			$options = [ 'Argentina', 'Australia', 'Bangladesh', 'Belgium', 'Bolivia', 'Cambodia', 'Canada', 'China', 'Colombia', 'Costa Rica',
+				'Denmark', 'Ecuador', 'El Salvador', 'Ethiopia', 'England', 'France', 'Germany', 'Guatemala', 'Haiti', 'India', 'Indonesia', 'Italy',
+				'Japan', 'Jordan', 'Kenya', 'Korea', 'Mexico', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Nigeria', 'Panama', 'Philippines',
+				'Portugal', 'Scotland', 'Senegal', 'South Africa', 'Sweden', 'Switzerland', 'Taiwan', 'Tanzania', 'Thailand', 'Turkey',
+				'United Kingdom', 'United States', 'Wales'
+			];
+			foreach ( $options as $text ) {
+				$value = 'incategory:' . str_replace( ' ', '_', $text );
+				$form .= '<option' . ( in_array( $value, $terms ) ? ' selected' : '' ) . ' value="' . $value . '">' . $text . '</option>';
+			}
+			$form .= '</select>';
+		}
+
+		$form .= '</div>';
+	}
+}

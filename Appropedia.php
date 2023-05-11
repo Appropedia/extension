77105b45a5dfd84ed9392157dd2215ae2e790228
Sophivorus <schenonef@gmail.com>
2023-05-11 08:10:54 -0700

    Add #arraymap

--- Appropedia.php
+++ Appropedia.php
@@ -11,6 +11,74 @@ class Appropedia {
 	}
 
 	/**
+	 * Main hook
+	 *
+	 * @param Parser $parser Parser object
+	 */
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook( 'arraymap', [ self::class, 'onFunctionHook' ], Parser::SFH_OBJECT_ARGS );
+	}
+
+	/**
+	 * This method is copied from Extension:PageForms
+	 * but we copy it here rather than enabling the extension
+	 * because it's the only thing we want from it
+	 */
+	public static function onFunctionHook() {
+		// Set variables
+		$value = isset( $args[0] ) ? trim( $frame->expand( $args[0] ) ) : '';
+		$delimiter = isset( $args[1] ) ? trim( $frame->expand( $args[1] ) ) : ',';
+		$var = isset( $args[2] ) ? trim( $frame->expand( $args[2], PPFrame::NO_ARGS | PPFrame::NO_TEMPLATES ) ) : 'x';
+		$formula = isset( $args[3] ) ? $args[3] : 'x';
+		$new_delimiter = isset( $args[4] ) ? trim( $frame->expand( $args[4] ) ) : ', ';
+		$conjunction = isset( $args[5] ) ? trim( $frame->expand( $args[5] ) ) : $new_delimiter;
+		// Unstrip some
+		$delimiter = $parser->getStripState()->unstripNoWiki( $delimiter );
+		// Let '\n' represent newlines, and '\s' represent spaces
+		$delimiter = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $delimiter );
+		$new_delimiter = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $new_delimiter );
+		$conjunction = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $conjunction );
+
+		if ( $delimiter == '' ) {
+			$values_array = preg_split( '/(.)/u', $value, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE );
+		} else {
+			$values_array = explode( $delimiter, $value );
+		}
+		$results_array = [];
+		// Add results to the results array only if the old value was
+		// non-null, and the new, mapped value is non-null as well
+		foreach ( $values_array as $old_value ) {
+			$old_value = trim( $old_value );
+			if ( $old_value == '' ) {
+				continue;
+			}
+			$result_value = $frame->expand( $formula, PPFrame::NO_ARGS | PPFrame::NO_TEMPLATES );
+			$result_value = str_replace( $var, $old_value, $result_value );
+			$result_value = $parser->preprocessToDom( $result_value, $frame->isTemplate() ? Parser::PTD_FOR_INCLUSION : 0 );
+			$result_value = trim( $frame->expand( $result_value ) );
+			if ( $result_value == '' ) {
+				continue;
+			}
+			$results_array[] = $result_value;
+		}
+		if ( $conjunction != $new_delimiter ) {
+			$conjunction = " " . trim( $conjunction ) . " ";
+		}
+		$result_text = "";
+		$num_values = count( $results_array );
+		for ( $i = 0; $i < $num_values; $i++ ) {
+			if ( $i == 0 ) {
+				$result_text .= $results_array[$i];
+			} elseif ( $i == $num_values - 1 ) {
+				$result_text .= $conjunction . $results_array[$i];
+			} else {
+				$result_text .= $new_delimiter . $results_array[$i];
+			}
+		}
+		return $result_text;
+	}
+
+	/**
 	 * Customize the logo and sidebar
 	 *
 	 * This hook changes the logo and sidebar depending on the category

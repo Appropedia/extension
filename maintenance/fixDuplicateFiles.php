5f05fd94054c461e7902f4cbe6f394e92e08ada8
Sophivorus <schenonef@gmail.com>
2023-12-13 08:00:46 -0600

    Start script but still doesn't do what it should

---
+++ maintenance/fixDuplicateFiles.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Fix duplicate files
+ */
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+use MediaWiki\MediaWikiServices;
+
+class AddCategoryData extends Maintenance {
+
+	public function execute() {
+
+		// Get files with duplicates
+		$dbr = wfGetDB( DB_REPLICA );
+		$result = $dbr->select( 'image', [ 'namespace' => NS_FILE, 'title' => 'MIN(img_name)', 'value' => 'count(*)', 'hash' => 'img_sha1' ], [], __METHOD__, [ 'GROUP BY' => 'img_sha1', 'HAVING' => 'count(*) > 1' ] );
+		foreach ( $result as $row ) {
+			$title = $row->title;
+			$hash = $row->hash;
+
+			// Get the duplicates
+			$result2 = $dbr->select( 'image', [ 'title' => 'img_name' ], [ 'img_sha1' => $hash ] );
+			foreach ( $result2 as $row2 ) {
+				$title = $row2->title;
+				$this->output( $title . PHP_EOL );
+
+				// @todo Delete all duplicates with no uses
+				// Making redirect won't work because the files are preserved even though the file pages become redirects
+			}
+			$this->output( PHP_EOL );
+		}
+	}
+}
+
+$maintClass = AddCategoryData::class;
+require_once RUN_MAINTENANCE_IF_MAIN;

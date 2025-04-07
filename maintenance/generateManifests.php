36bc3b0e0ae2c7a107576106fc1a6fe28e75f971
Sophivorus <schenonef@gmail.com>
2025-04-07 09:59:57 -0500

    Fix and improve

--- maintenance/generateManifests.php
+++ maintenance/generateManifests.php
@@ -11,52 +11,47 @@ if ( $IP === false ) {
 }
 require_once "$IP/maintenance/Maintenance.php";
 
-use MediaWiki\MediaWikiServices;
-use Sophivorus\EasyWiki;
-
 class GenerateManifests extends Maintenance {
 
 	public function execute() {
 
-		// Delete manifests
-		exec( 'rm -f /home/appropedia/public_html/manifests/*' );
-		
-		// Get all projects
-		$api = new EasyWiki( 'https://www.appropedia.org/w/api.php' );
-		$params = [ 'action' => 'askargs', 'conditions' => 'Type::Project' ];
-		$result = $api->get( $params );
-		//var_dump( $result ); exit; // Uncomment to debug
-		$results = $api->find( 'results', $result );
-		
-		// Make manifest for each project
-		foreach ( $results as $title => $values ) {
+		// Make sure the manifests dir exists and is empty
+		$manifests = '/home/appropedia/public_html/manifests';
+		if ( is_dir( $manifests ) ) {
+			exec( "rm -f $manifests/*" );
+		} else {
+			mkdir( $manifests );
+		}
+
+		// Get all the projects
+		$titles = [];
+		$category = Category::newFromName( 'Projects' );
+		$members = $category->getMembers();
+		foreach ( $members as $title ) {
+			$title = $title->getFullText();
+			if ( preg_match( '#/[a-z]{2}$#', $title ) ) {
+				continue; // Skip automatic translations, for now
+			}
+			$titles[] = $title;
+		}
+		//var_dump( $titles ); exit; // Uncomment to debug
+
+		// Make a manifest for each project
+		$files = [];
+		foreach ( $titles as $title ) {
 			echo $title . PHP_EOL;
-			$hash = md5( $title );
 			$titlee = str_replace( ' ', '_', $title ); // Basic encoding
 			$url = "https://www.appropedia.org/scripts/generateOpenKnowHowManifest.php?title=$titlee";
 			$manifest = file_get_contents( $url );
-			$manifest = trim( $manifest );
-			file_put_contents( "../manifests/$hash.yaml", $manifest );
-			//exit; // Uncomment to debug
+			$hash = md5( $title );
+			$files[] = "https://www.appropedia.org/manifests/$hash.yaml";
+			file_put_contents( "$manifests/$hash.yaml", $manifest );
+			//break; // Uncomment to debug
 		}
-		
-		// Make list.json index
-		$dir = '/home/appropedia/public_html/manifests/';
-		if ( is_dir( $dir ) ) {
-			$list = [];
-			if ( $dir_handle = opendir( $dir ) ) {
-				while ( ( $file = readdir( $dir_handle ) ) !== false ) {
-					if ( substr( $file, 0, 1 ) === '.' ) {
-						continue; // Skip hidden files
-					}
-					$url = 'https://www.appropedia.org/manifests/' . $file;
-					$list[] = $url;
-				}
-			}
-			echo 'list.json' . PHP_EOL;
-			$json = json_encode( $list, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT );
-			file_put_contents( $dir . 'list.json', $json );
-		}
+
+		// Make the list.json index
+		$json = json_encode( $files, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT );
+		file_put_contents( $manifests . '/list.json', $json );
 	}
 }
 

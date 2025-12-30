8208142834706a2dacf7cc7302a2968d6d1a20f6
Sophivorus <schenonef@gmail.com>
2025-12-30 09:16:16 -0600

    Add maintenance scripts

---
+++ maintenance/addLicense.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This maintenance script adds the license to all pages that don't have it already
+ */
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+use MediaWiki\MediaWikiServices;
+
+class AddLicense extends Maintenance {
+
+	public function execute() {
+
+		$services = MediaWikiServices::getInstance();
+		$provider = $services->getConnectionProvider();
+		$dbr = $provider->getReplicaDatabase();
+		$ids = $dbr->newSelectQueryBuilder()
+			->field( 'page_id' )
+			->from( 'page' )
+			->where( [ 'page_is_redirect' => 0, 'page_namespace' => 0 ] )
+			->fetchFieldValues();
+
+		$count = 0;
+		$total = count( $ids );
+		$factory = $services->getWikiPageFactory();
+		foreach ( $ids as $id ) {
+			$count++;
+
+			$title = Title::newFromID( $id );
+			if ( $title->isSubpage() ) {
+				continue;
+			}
+			$revision = $services->getRevisionLookup()->getFirstRevision( $title );
+			$timestamp = $revision->getTimestamp();
+			$year = substr( $timestamp, 0, 4 );
+			$year = intval( $year );
+			if ( $year > 2019 ) {
+				continue;
+			}
+			$url = $title->getFullURL();
+			$page = $factory->newFromTitle( $title );
+			$revision = $page->getRevisionRecord();
+			$content = $revision->getContent( 'main' );
+			$text = $content->getText();
+			$text = trim( $text );
+			if ( preg_match( '/{{Page data[^}]*\| *license *= *.+/', $text ) ) {
+				continue;
+			} else if ( preg_match( '/{{Page data}}/', $text ) ) {
+				$pattern = 1;
+				$text = preg_replace( '/{{Page data}}/', "{{Page data\n| license = CC-BY-SA-3.0\n}}", $text );
+			} else if ( preg_match( '/{{Page data/', $text ) ) {
+				$pattern = 2;
+				$text = preg_replace( '/{{Page data/', "{{Page data\n| license = CC-BY-SA-3.0", $text );
+			} else if ( preg_match( '/\[\[Category:.*\]\]$/s', $text ) ) {
+				$pattern = 3;
+				$text = preg_replace( '/\[\[Category:.*\]\]$/s', "{{Page data\n| license = CC-BY-SA-3.0\n}}\n\n$0", $text );
+			} else {
+				$pattern = 4;
+				$text .= "\n\n{{Page data\n| license = CC-BY-SA-3.0\n}}";
+			}
+			$percent = round( $count / $total * 100, 2 );
+			$this->output( "$percent% Add license (pattern $pattern): $url" . PHP_EOL );
+
+			// Save the page
+			$content = ContentHandler::makeContent( $text, $title );
+			$user = User::newSystemUser( User::MAINTENANCE_SCRIPT_USER, [ 'steal' => true ] );
+			$updater = $page->newPageUpdater( $user );
+			$updater->setContent( 'main', $content );
+			$comment = CommentStoreComment::newUnsavedComment( 'Add license' );
+			$updater->saveRevision( $comment, EDIT_SUPPRESS_RC | EDIT_FORCE_BOT | EDIT_MINOR | EDIT_INTERNAL );
+		}
+	}
+}
+
+$maintClass = AddLicense::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ maintenance/removeDisplayTitle.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This maintenance script removes display titles that are identical to the real title
+ */
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+use MediaWiki\MediaWikiServices;
+
+class RemoveDisplayTitle extends Maintenance {
+
+	public function execute() {
+
+		$services = MediaWikiServices::getInstance();
+		$provider = $services->getConnectionProvider();
+		$dbr = $provider->getReplicaDatabase();
+		$ids = $dbr->newSelectQueryBuilder()
+			->field( 'page_id' )
+			->from( 'page' )
+			->where( [ 'page_is_redirect' => 0, 'page_namespace' => 0 ] )
+			->fetchFieldValues();
+
+		$count = 0;
+		$total = count( $ids );
+		$factory = $services->getWikiPageFactory();
+		foreach ( $ids as $id ) {
+			$count++;
+
+			$title = Title::newFromID( $id );
+			$url = $title->getFullURL();
+			$page = $factory->newFromTitle( $title );
+			$revision = $page->getRevisionRecord();
+			$content = $revision->getContent( 'main' );
+			$text = $content->getText();
+			if ( !preg_match( '/{{Page data[^}]*\| *title *= *(.+)/', $text, $matches ) ) {
+				continue;
+			}
+			$displayTitle = $matches[1];
+			$subpageText = $title->getSubpageText();
+			if ( $displayTitle != $subpageText ) {
+				continue;
+			}
+			$text = preg_replace( '/{{Page data([^}]*)\| *title *= *(.+)\n/', '{{Page data$1', $text );
+
+			// Output the progress
+			$percent = round( $count / $total * 100, 2 );
+			$this->output( "$percent% Remove redundant display title: $url" . PHP_EOL );
+
+			// Save the page
+			$content = ContentHandler::makeContent( $text, $title );
+			$user = User::newSystemUser( User::MAINTENANCE_SCRIPT_USER, [ 'steal' => true ] );
+			$updater = $page->newPageUpdater( $user );
+			$updater->setContent( 'main', $content );
+			$comment = CommentStoreComment::newUnsavedComment( 'Remove redundant display title' );
+			$updater->saveRevision( $comment, EDIT_SUPPRESS_RC | EDIT_FORCE_BOT | EDIT_MINOR | EDIT_INTERNAL );
+		}
+	}
+}
+
+$maintClass = RemoveDisplayTitle::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
